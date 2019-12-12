541514047765c52af77eea8dd57de439c26406db
Blake Prouty <bpro0821@gmail.com>
2019-12-11 19:24:13 -0600

    Update recipe_batches.py

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,6 +3,24 @@
 import math
 
 def recipe_batches(recipe, ingredients):
+    min_batch = None
+    for ingredient in recipe:
+        if ingredient in ingredients:
+            batches = ingredients[ingredient] // recipe[ingredient]
+            if min_batch is None:
+                min_batch = batches
+            else:
+                min_batch = min(batches, min_batch)
+        else:
+            return 0
+    return min_batch
+  
+recipe_batches(
+    {'milk': 2, 'sugar': 40, 'butter': 20},
+    {'milk': 5, 'sugar': 120, 'butter': 500}
+)
+
+def recipe_batches(recipe, ingredients):
   pass 
 
 
